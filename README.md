f19e598ea5bcec7f3a5c231ced2e0ab74e5b9c9f
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2019-05-03 11:59:20 -0600

    Initial commit with Readme and tagline

---
+++ README.md
@@ -0,0 +1,3 @@
+# Forbidden Overlay
+This project is an integration with a google sheet containing map information
+for the TTRPG Forbidden Lands by [Fria Ligan](https://frialigan.se/en/startpage/).
---
+++ src/resetableQueue.js
@@ -0,0 +1,32 @@
+export default class ResetableQueue {
+  constructor(elements) {
+    this.items = elements || [];
+    this.originalItems = Object.assign([], this.items)
+    this.resetAtCount = 0;
+  }
+
+  reset() {
+    this.items = Object.assign([], this.originalItems)
+  }
+
+  queue(el) {
+    this.items.push(el);
+  }
+
+  dequeue() {
+    let reset = this.needsReseting() ? this.reset() : null
+    return this.items.shift();
+  }
+
+  needsReseting() {
+    return this.items.length <= this.resetAtCount
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  front() {
+    return this.isEmpty() ? 'Queue is empty' : this.items[0];
+  }
+}
